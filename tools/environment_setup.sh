af7cf70e6548dc5ee6f4bec0f771037f25472a48
Hong Ooi <hongooi@microsoft.com>
2020-06-30 08:01:01 +1000

    pin pmdarima, matplotlib versions (#212)
    * pin pmdarima to 1.1.1
    
    * try 1.2.0
    
    * fix envsetup, revert to ==1.1.1
    
    * also update test yml
    
    * fix this script as well
    
    * pin matplotlib
    
    Co-authored-by: Chenhui Hu <chenhhu@microsoft.com>

--- tools/environment_setup.sh
+++ tools/environment_setup.sh
@@ -17,7 +17,7 @@ eval "$(conda shell.bash hook)" && conda activate forecasting_env
 pip install -e fclib
 
 # Install ray (available only on Linux and MacOS)
-pip install ray>=0.8.2 
+pip install 'ray>=0.8.2'
 
 # Register conda environment in Jupyter
 python -m ipykernel install --user --name forecasting_env
